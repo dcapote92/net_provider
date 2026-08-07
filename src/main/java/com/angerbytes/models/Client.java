fd09fb31f31dd344aad4d7ccb888674bd73b9036
dcapote92 <danicapote92@gmail.com>
2026-08-07 17:01:10 -0300

    fix: setting client data to basics

--- src/main/java/com/angerbytes/models/Client.java
+++ src/main/java/com/angerbytes/models/Client.java
@@ -1,26 +1,28 @@
 package com.angerbytes.models;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Client {
+
+    private final UUID id;
     private String name;
-    private String cpf;
-    private final String birthDate;
     private String email;
     private String phone;
-    private String address;
 
-    public Client(String name, String cpf, String birthDate, String email, String phone, String address) {
+    public Client(String name) {
+        this.id = UUID.randomUUID();
         this.name = name;
-        this.cpf = cpf;
-        this.birthDate = birthDate;
-        this.email = email;
-        this.phone = phone;
-        this.address = address;
     }
 
-    public String getBirthDate() {
-        return birthDate;
+    public Client(String name, String email) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getName() {
@@ -29,14 +31,6 @@ public class Client {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 
     public String getEmail() {
@@ -55,53 +49,26 @@ public class Client {
         this.phone = phone;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Client client = (Client) o;
-        return Objects.equals(cpf, client.cpf) && Objects.equals(email, client.email);
+        return Objects.equals(id, client.id) && Objects.equals(email, client.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cpf, email);
+        return Objects.hash(id, email);
     }
 
     @Override
     public String toString() {
         return "Client{" +
                 "name='" + name + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", birthDate='" + birthDate + '\'' +
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
-                ", address='" + address + '\'' +
                 '}';
     }
-
-    public String cpfMask(){
-        return String.format("%s.%s.%s-%s",
-                cpf.substring(0,3),
-                cpf.substring(3,6),
-                cpf.substring(6,9),
-                cpf.substring(9,11)
-        );
-    }
-
-    public String phoneMask(){
-        phone = phone.startsWith("0") ? phone.substring(1) : phone;
-        return String.format("(%s) %s-%s",
-                phone.substring(0,2),
-                phone.substring(2,6),
-                phone.substring(6)
-        );
-    }
 }
+
+
