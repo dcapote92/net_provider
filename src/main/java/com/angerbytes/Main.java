95e0ced7ae35e9593cb8ed7eee455263ab3f98fc
dcapote92 <danicapote92@gmail.com>
2026-08-08 16:10:20 -0300

    fix: uml plan name var

--- src/main/java/com/angerbytes/Main.java
+++ src/main/java/com/angerbytes/Main.java
@@ -8,6 +8,7 @@ public class Main {
     static void main() {
         Client cl1 = new Client("Jhon Doe");
         cl1.setPhone(MaskText.phone("085920005853"));
+        cl1.setEmail("daniel@email.com");
         System.out.println(cl1);
     }
 }
